5c208e4ee5f030ab4bd76f6f42b261aa1b67b429
Vishnu Vardhan Reddy <vishnuvardhanreddy@Vishnus-MacBook-Air.local>
2025-12-11 00:22:51 +0530

    Fix: Add AnnouncementHistory route and remove xlsx from dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -36,7 +36,7 @@ function App() {
         <Route 
           path="/history" 
           element={
-            <PrivateRoute role="teacher">
+            <PrivateRoute>
               <AnnouncementHistory />
             </PrivateRoute>
           } 
